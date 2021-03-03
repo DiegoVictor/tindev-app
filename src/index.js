c53ce234ab730d043d9e6f3beaf2701ebe5dc8bf
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-03-03 00:12:57 -0300

    Remove yellow box supress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { YellowBox } from 'react-native';
 
 import Navigation from '~/routes';
 
-YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
 
 export default () => {
   return <Navigation />;
