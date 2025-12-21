106aa44070499e33ddf424139d84bd943feb26c4
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-12-21 19:03:03 -0300

    Remove custom resolver for socket.io-client from tests

--- tests/custom_resolver.js
+++
@@ -1,6 +0,0 @@
-module.exports = (request, options) => {
-  if (request.search(/socket\.io-client/gi) > -1) {
-    return `${options.rootDir}/mocks/socket.io-client.js`;
-  }
-  return options.defaultResolver(request, options);
-};
